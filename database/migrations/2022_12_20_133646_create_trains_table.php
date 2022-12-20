ce4135d622cf43d261422ec8b7704caad62194a4
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-12-20 14:51:01 +0100

    add database

---
+++ database/migrations/2022_12_20_133646_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 150);
+            $table->string('departure_station', 200);
+            $table->string('arrival_station', 200);
+            $table->time('departure_time', $precision = 2);
+            $table->time('arrival_time', $precision = 2);
+            $table->string('train_no');
+            $table->integer('carriage_no')->nullable();
+            $table->boolean('in_time')->nullable();
+            $table->boolean('deleted')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
