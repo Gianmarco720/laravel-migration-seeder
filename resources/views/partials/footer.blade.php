cf1e85affb897edbe6fdccb5268af65bf466a73f
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-12-20 16:52:21 +0100

    final commit - end ex

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,21 @@
-<footer>
-    <h1>Footer</h1>
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Legal</h3>
+                <ul class="list-unstyled">
+                    <li>Privacy Policy</li>
+                    <li>Terms and Conditions</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
